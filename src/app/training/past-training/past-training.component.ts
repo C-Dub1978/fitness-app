5022a6721fcbde452e81be66b315d6e8bd7854d9
TransientCW <chris@myubercode.com>
2018-11-24 13:59:06 -0700

    finished previous workouts mat-table with sorting/filtering, moving on to firebase

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,6 +1,6 @@
 import { TrainingService } from './../training.service';
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 import { Exercise } from '../exercise.model';
 
 @Component({
@@ -8,11 +8,22 @@ import { Exercise } from '../exercise.model';
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.scss']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements AfterViewInit, OnInit {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   dataSource = new MatTableDataSource<Exercise>();
   displayedColumns = ['date', 'name', 'duration', 'calories', 'status'];
 
   constructor(private trainingService: TrainingService) {}
+
+  doFilter(value: string) {
+    this.dataSource.filter = value.trim().toLowerCase();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCanceledExercises();
